3d410097f6a8155f2a429af80206c07b8eddf477
Nisha <nisha@zipcodes-mbp-8.myfiosgateway.com>
2021-08-21 22:42:53 -0400

    Edited

--- src/main/java/io/zipcoder/UnsafeCopier.java
+++ src/main/java/io/zipcoder/UnsafeCopier.java
@@ -10,10 +10,14 @@ public class UnsafeCopier extends Copier {
     }
 
     public void run() {
-        while (stringIterator.hasNext()){
-            if(stringIterator.hasNext()){
-                copied += stringIterator.next()+ " ";
+        try {
+            while (stringIterator.hasNext()) {
+                if (stringIterator.hasNext()) {
+                    copied += stringIterator.next() + " ";
+                }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
